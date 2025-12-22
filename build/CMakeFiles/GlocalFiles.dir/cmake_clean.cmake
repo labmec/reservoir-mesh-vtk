4742ca4bc4435bfb544d790998d84d4d3692e2c4
ErickOlima <e240197@dac.unicamp.br>
2025-12-22 12:19:41 -0300

    Ignore build, VS Code config and Zone.Identifier files

--- build/CMakeFiles/GlocalFiles.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GlocalFiles"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GlocalFiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
