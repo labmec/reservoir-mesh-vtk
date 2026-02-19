4b32b05c5099031688dce2988e04e5e82eb0471a
g-taraschi <gitaraschi@gmail.com>
2026-02-19 14:13:28 -0300

    fix: remove forcing function from MP mesh

--- DarcySpaces.cpp
+++ DarcySpaces.cpp
@@ -30,7 +30,7 @@ int gthreads = 0;
 // gthreads = std::thread::hardware_concurrency(); // Number of threads for parallel execution
 // matsp.SetNumThreads(gthreads);
 
-// Exact solution
+// Exact solution (if needed)
 TLaplaceExample1 gexact;
 
 // Permeability
@@ -58,10 +58,79 @@ TPZGeoMesh *createGeoMesh(std::string file);
 
 // Marks pyramid elements in the geometric mesh
 void MarkPyramids(TPZGeoMesh *gmesh);
+
+// Divides pyramid elements in the geometric mesh into tetrahedra
 void DividePyramids(TPZGeoMesh *gmesh);
 
-// Marks pyramid elements in the geometric mesh
+// Marks tetrahedral elements in the geometric mesh
 void MarkETetraedros(TPZGeoMesh *gmesh);
+
+// Computational meshes (H1 and Mixed)
+TPZCompMesh* createCompMesh(TPZGeoMesh* gmesh);
+TPZMultiphysicsCompMesh* createCompMeshMixed(TPZGeoMesh* gmesh, int pOrder);
+
+// =============
+// Main function
+// =============
+
+int main(int argc, char *const argv[]) {
+  
+  #ifdef PZ_LOG
+    TPZLogger::InitializePZLOG();
+  #endif
+  gRefDBase.InitializeRefPatterns();
+  
+  cout << "Reading mesh and creating geometric mesh..." << endl;
+
+  // Read .msh file to create geometric mesh
+  TPZGeoMesh *gmesh = createGeoMesh("Moving_Reservoircopy.msh");
+
+  // mark pyramids
+  // MarkPyramids(gmesh);
+  DividePyramids(gmesh);
+
+  // H1 solver
+  cout << "Creating computational mesh H1..." << endl;
+  TPZCompMesh *cmesh = createCompMesh(gmesh);
+  TPZLinearAnalysis an(cmesh);
+  TPZSSpStructMatrix<STATE, TPZStructMatrixOR <STATE >> matsp(cmesh);
+  matsp.SetNumThreads(gthreads);
+  an.SetStructuralMatrix(matsp);
+  TPZStepSolver<STATE> step;
+  step.SetDirect(ECholesky);
+  an.SetSolver(step);
+  an.Run();
+
+  // Mixed solver (H(div)-L2)
+  cout << "Creating computational mesh mixed..." << endl;
+  TPZMultiphysicsCompMesh *cmeshMixed = createCompMeshMixed(gmesh, 1);
+  TPZLinearAnalysis anMixed(cmeshMixed);
+  TPZSSpStructMatrix<STATE> matMixed(cmeshMixed);
+  matMixed.SetNumThreads(gthreads);
+  anMixed.SetStructuralMatrix(matMixed);
+  TPZStepSolver<STATE> stepMixed;
+  stepMixed.SetDirect(ELDLt);
+  anMixed.SetSolver(stepMixed);
+  anMixed.Run();
+
+  {
+    constexpr int vtkRes{0}; 
+    const std::string plotfile = "H1_solution"; 
+    TPZManVector<std::string, 2> fields = {"Pressure", "Flux"};
+    auto vtk = TPZVTKGenerator(cmesh, fields, plotfile, vtkRes);  
+    vtk.Do();
+  }
+
+  {
+    constexpr int vtkRes{0}; 
+    const std::string plotfile = "Mixed_solution"; 
+    TPZManVector<std::string, 2> fields = {"Pressure", "Flux"};
+    auto vtk = TPZVTKGenerator(cmeshMixed, fields, plotfile, vtkRes);  
+    vtk.Do();
+  }
+
+  return 0;
+}
 
 // =========
 // Functions
@@ -122,7 +191,8 @@ void MarkPyramids(TPZGeoMesh *gmesh) {
 }
 
 void DividePyramids(TPZGeoMesh *gmesh) {
-    auto refpatter = gRefDBase.FindRefPattern("PyrTwoTets");
+  auto refpatter = gRefDBase.FindRefPattern("PyrTwoTets");
+
   if (!refpatter) {
     std::cout << "Refinement pattern for pyramids not found!" << std::endl;
     DebugStop();
@@ -136,8 +206,6 @@ void DividePyramids(TPZGeoMesh *gmesh) {
       geoel->SetRefPattern(refpatter);
       TPZManVector<TPZGeoEl *> el(0);
       geoel->Divide(el);
-     
-     
     }
   }
   gmesh->BuildConnectivity();
@@ -159,8 +227,6 @@ void MarkETetraedros(TPZGeoMesh *gmesh) {
   cout << "Tetrahedral marker callled ";
 }
 
-// MALHA COMPUTACIONAL
-
 TPZCompMesh* createCompMesh(TPZGeoMesh* gmesh) {
   TPZCompMesh* cmesh = new TPZCompMesh(gmesh);
 
@@ -173,28 +239,26 @@ TPZCompMesh* createCompMesh(TPZGeoMesh* gmesh) {
   cmesh -> InsertMaterialObject(material);
   
   TPZFMatrix<REAL> val1(1,1,0.);
-  TPZManVector<REAL,1> val2(1, 9.);
+  TPZManVector<REAL,1> val2(1, 0.);
   
   int diritype = 0, neumanntype = 1, robinntype = 2;
   
-  
   val2[0] = 3.;
-    cmesh ->InsertMaterialObject(
-    material ->CreateBC(material, EFarfield, diritype, val1, val2));
+  cmesh ->InsertMaterialObject(
+  material ->CreateBC(material, EFarfield, diritype, val1, val2));
 
   val2[0] = 1.;
   cmesh -> InsertMaterialObject(
-    material ->CreateBC(material, ECylinder, diritype, val1, val2));
+  material ->CreateBC(material, ECylinder, diritype, val1, val2));
 
   val2[0] = 0.;
   cmesh -> InsertMaterialObject(
-    material ->CreateBC(material, ETampa, neumanntype, val1, val2));
+  material ->CreateBC(material, ETampa, neumanntype, val1, val2));
   
   cmesh->AutoBuild();
   cmesh->CleanUpUnconnectedNodes();
   
   return cmesh;
-
 }
 
 TPZMultiphysicsCompMesh* createCompMeshMixed(TPZGeoMesh *gmesh, int order) {
@@ -206,18 +270,18 @@ TPZMultiphysicsCompMesh* createCompMeshMixed(TPZGeoMesh *gmesh, int order) {
   cmeshFlux->SetDefaultOrder(order);
   cmeshFlux->SetAllCreateFunctionsHDiv();
 
-  // Create boundary conditions
-  TPZManVector<REAL,1> val2(1,3.); // Part that goes to the RHS vector
-  TPZFMatrix<REAL> val1(1,1,0.); // Part that goes to the Stiffnes matrix
-
   TPZNullMaterial<STATE> *mat = new TPZNullMaterial(EDomain, gmesh->Dimension());  
   cmeshFlux->InsertMaterialObject(mat);
+
+  // Create boundary conditions
+  TPZManVector<REAL,1> val2(1,0.); // Part that goes to the RHS vector
+  TPZFMatrix<REAL> val1(1,1,0.); // Part that goes to the Stiffnes matrix
 
   TPZBndCondT<REAL> *bcond = mat->CreateBC(mat, ECylinder, 1, val1, val2);
   cmeshFlux->InsertMaterialObject(bcond);
 
   val2[0] = 1.;
-  bcond = mat ->CreateBC(mat, EFarfield, 0, val1, val2);
+  bcond = mat ->CreateBC(mat, EFarfield, 1, val1, val2);
   cmeshFlux->InsertMaterialObject(bcond);
 
   val2[0] = 0.;
@@ -256,22 +320,20 @@ TPZMultiphysicsCompMesh* createCompMeshMixed(TPZGeoMesh *gmesh, int order) {
 
   TPZMultiphysicsCompMesh *cmesh = new TPZMultiphysicsCompMesh(gmesh);
   cmesh->SetDimModel(gmesh->Dimension());
-  cmesh->SetDefaultOrder(1); // Needed? Wasn't it already set in the atomic meshes?
+  cmesh->SetDefaultOrder(order);
   cmesh->ApproxSpace().Style() = TPZCreateApproximationSpace::EMultiphysics;
 
   // Add materials (weak formulation)
   TPZMixedDarcyFlow *matDarcy = new TPZMixedDarcyFlow(EDomain, gmesh->Dimension());  
   matDarcy->SetConstantPermeability(1.0);
-  matDarcy->SetForcingFunction(gexact.ForceFunc(),4);
-  matDarcy->SetExactSol(gexact.ExactSolution(),4);
   cmesh->InsertMaterialObject(matDarcy);
 
   // Create, set and add boundary conditions
-  val2[0] = 3.;
+  val2[0] = 1.;
   bcond = matDarcy->CreateBC(matDarcy, ECylinder, 0, val1, val2);
   cmesh->InsertMaterialObject(bcond);
 
-  val2[0] = 1.;
+  val2[0] = 3.;
   bcond = matDarcy->CreateBC(matDarcy, EFarfield, 0, val1, val2);
   cmesh->InsertMaterialObject(bcond);
   
@@ -288,87 +350,4 @@ TPZMultiphysicsCompMesh* createCompMeshMixed(TPZGeoMesh *gmesh, int order) {
   cmesh->BuildMultiphysicsSpace(active, cmeshes);
 
   return cmesh;
-}
-
-
-// =============
-// Main function
-// =============
-
-int main(int argc, char *const argv[]) {
-  
-  #ifdef PZ_LOG
-    TPZLogger::InitializePZLOG();
-  #endif
-  gRefDBase.InitializeRefPatterns();
-  
-  cout << "Reading mesh and creating geometric mesh..." << endl;
-
-  // Read .msh file to create geometric mesh
-  TPZGeoMesh *gmesh = createGeoMesh("Moving_Reservoircopy.msh");
-
-  // mark pyramids
-  // MarkPyramids(gmesh);
-  // DividePyramids(gmesh);
-
-  cout << "Creating computational mesh H1..." << endl;
-  TPZCompMesh *cmesh = createCompMesh(gmesh);
-  // cmesh->Print(std::cout);
-  TPZLinearAnalysis an(cmesh);
-  TPZSSpStructMatrix<STATE, TPZStructMatrixOR <STATE >> matsp(cmesh);
-  matsp.SetNumThreads(gthreads);
-  an.SetStructuralMatrix(matsp);
-  TPZStepSolver<STATE> step;
-  step.SetDirect(ECholesky);
-  
-
-  an.SetSolver(step);
-  an.Run();
-
-  cout << "Creating computational mesh mixed..." << endl;
-  TPZMultiphysicsCompMesh *cmeshMixed = createCompMeshMixed(gmesh, 1);
-  // cmeshMixed->Print(std::cout);
-  TPZLinearAnalysis anMixed(cmeshMixed);
-  TPZSSpStructMatrix<STATE> matmixed(cmeshMixed);
-  matmixed.SetNumThreads(gthreads);
-  anMixed.SetStructuralMatrix(matmixed);
-  TPZStepSolver<STATE> stepMixed;
-  stepMixed.SetDirect(ELDLt);
- 
-  anMixed.SetSolver(stepMixed);
-  anMixed.Run();
-
-  // Plotting
-  constexpr int vtkRes{0}; 
-
-  {
-    const std::string plotfile = "postproct"; 
-    TPZManVector<std::string, 2> fields = {"Pressure", "Flux"};
-    auto vtk = TPZVTKGenerator(cmeshMixed, fields, plotfile, vtkRes);  
-    vtk.Do();
-  }
-
-  {
-    const std::string plotfile = "postproct_mixed"; 
-    TPZManVector<std::string, 2> fields = {"Pressure", "Flux"};
-    auto vtk = TPZVTKGenerator(cmeshMixed, fields, plotfile, vtkRes);  
-    vtk.Do();
-  }
-
-
-  // an.Solution().Print("Solution");
-
-  // cout<< "Computational mesh solution: "<<endl;
-  
-  // cmesh->Solution().Print("CompMesh Solution");
-  
- 
- 
-  // TPZRefPatternTools::RefinePyramids(gmesh, EMarkedPyramide, 1);
-  
-  // Plot gmesh
-  // std::ofstream out("geomesh.vtk");
-  // TPZVTKGeoMesh::PrintGMeshVTK(gmesh, out);
-
-  return 0;
 }
